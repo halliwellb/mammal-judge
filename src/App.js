e75af9c87e509ad6e0353bde7a31a7ce79451097
Ben Halliwell <bkhalliwell@outlook.com>
2023-04-12 00:39:02 +0000

    new branch

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
   console.log(imageUrls);
   return (
     <>
-      <Header title='Mammal Judge'/>
-      <Main heading="Who's you're favorite Mammal?" imageUrls={imageUrls}/>
-      <Footer message='Let us know your favorite!'/>
+      <Header title='Mammal Judge' />
+      <Main heading="Who's you're favorite Mammal?" imageUrls={imageUrls} />
+      <Footer message='Let us know your favorite!; Created by B. Halliwell' />
     </>
   );
 }
